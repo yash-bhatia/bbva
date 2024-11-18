fc722a11ae3eac1456618c37bea9f0a5a208f830
lakshya aggarwal <laggarwal@adobe.com>
2024-11-18 17:44:22 +0530

    Update Banner component

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,7 +1,10 @@
 export default function decorate(block) {
   [...block.children].forEach((row) => {
     if (row.textContent.trim().startsWith('#')) {
-      row.style.backgroundColor = row.textContent.trim();
+      const bannerContainer = document.querySelector('.section.banner-container.bannerimage-container');
+      if (bannerContainer) {
+        bannerContainer.style.backgroundColor = row.textContent.trim(); // Set your desired background color here
+      }
       row.textContent = '';
     }
   });
